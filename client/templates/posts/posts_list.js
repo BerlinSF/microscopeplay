0b8a43da5acc4edbf958a02a3470bddfa829e54c
Alex Scholz <alexeverywhere@gmail.com>
2015-04-14 21:07:47 +0200

    init commit

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,26 @@
+Template.postsList.helpers({
+  posts: function() {
+    return Posts.find();
+  }
+});
+
+
+/* dummy data from start
+
+var postsData = [
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  }, 
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  }, 
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
+}); */
